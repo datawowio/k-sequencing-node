8d81c79c66e9267de2459c4fdc19166081b198da
aun <aun.sira@gmail.com>
2018-03-27 15:36:11 +0700

    Add kiyo-text endpoint

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,8 @@ module.exports = {
   HEADER: {
     AUTHORIZATION: 'Authorization',
   },
-  URL: 'https://k-sequencing.datawow.io/api/',
+  KIYO_IMAGE_URL: 'https://k-sequencing.datawow.io/api/',
+  KIYO_TEXT_URL: 'https://kiyo-text.datawow.io/api/v1',
   ENDPOINT: {
     PHOTO_TAGS: 'images/photo_tags',
     CHOICES: 'images/choices',
