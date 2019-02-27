9ace73b1b54401265ac6e974022cdb9b300aa4e3
Sira Sarakit (Aun) <aun.sira@gmail.com>
2019-02-27 15:00:34 +0700

    Add Ai Consensus action calls (#4)
    * feat: Add AI Consensus actions
    
    * fix: Fixed url
    
    * test: Update fixtures
    
    * docs: Update README

---
+++ tests/operations/images/ai-consensuses.test.js
@@ -0,0 +1,54 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const axios = require('axios');
+const _ = require('lodash');
+const HttpStatus = require('http-status');
+const { listAiConsensus, createAiConsensus } = require('../../fixtures/images/ai-consensuses');
+
+sinon.assert.expose(chai.assert, { prefix: '' });
+const assert = chai.assert;
+
+const aiConsensus = require('../../../src/operations/images/aiConsensus');
+const httpClient = require('../../../src/httpClient');
+
+const listOptions = {
+  id: 1,
+};
+
+const createOptions = {
+  data: 'https://assets-cdn.github.com/images/modules/open_graph/github-mark.png',
+  custom_id: '1',
+};
+
+const PROJECT_KEY = 'project-key';
+
+describe('operations/aiConsensus', function () {
+  let sandbox;
+  let callGetStub;
+  let callPostStub;
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+    callGetStub = sandbox.stub(httpClient, 'callGet');
+    callPostStub = sandbox.stub(httpClient, 'callPost');
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it('should get list of AI Consensus', async function () {
+    callGetStub.resolves({ data: listAiConsensus });
+    const result = await aiConsensus.list({ token: PROJECT_KEY, data: listOptions });
+    const parsed = JSON.parse(result.data);
+    assert.isArray(parsed.data.images);
+    assert.equal(parsed.meta.code, HttpStatus.OK);
+  });
+
+  it('should create AI Consensus', async function () {
+    callPostStub.resolves({ data: createAiConsensus });
+    const result = await aiConsensus.create({ token: PROJECT_KEY, data: createOptions });
+    const parsed = JSON.parse(result.data);
+    assert.isObject(parsed.data);
+    assert.equal(parsed.meta.code, HttpStatus.CREATED);
+  });
+});
