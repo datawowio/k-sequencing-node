6a700ab8087832994efe6cb176efbe787e5c8dcf
NSLog0 <prachaya.su@gmail.com>
2019-04-04 10:44:28 +0700

    change base url

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,8 @@ module.exports = {
   HEADER: {
     AUTHORIZATION: 'Authorization',
   },
-  KIYO_IMAGE_URL: 'https://kiyo-image.datawow.io/api/v1/',
-  KIYO_TEXT_URL: 'https://kiyo-text.datawow.io/api/v1/',
+  KIYO_IMAGE_URL: 'https://image.datawow.io/api/v1/',
+  KIYO_TEXT_URL: 'https://text.datawow.io/api/v1/',
   ENDPOINT: {
     IMAGES: {
       PHOTO_TAGS: 'images/photo_tags',
